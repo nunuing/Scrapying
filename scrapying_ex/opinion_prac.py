0cd7dc121eea85217c926dee2e12c4ff191179d9
eycho <chw3872@gmail.com>
2024-04-18 21:48:08 +0900

    scikit-learn

--- scrapying_ex/opinion_prac.py
+++ scrapying_ex/opinion_prac.py
@@ -9,4 +9,14 @@ okt = Okt()
 
 train_df = pd.read_csv('myproject/에스쁘아 톤페어링 치크 9.6g + 톤페어링 하이라이터 9g .csv', encoding='utf-8')
 
-print(train_df)
+print(train_df.info())
+
+# 한글 외 문자 제거(옵션)
+# ‘ㄱ ~‘힣’까지의 문자를 제외한 나머지는 공백으로 치환, 영문: a-z| A-Z
+import re # 정규식을 사용하기 위해 re 모듈을 임포트
+train_df['리뷰'] = train_df['리뷰'].apply(lambda x : re.sub(r'[^ ㄱ-ㅣ가-힣]+', " ", x))
+
+review = train_df['리뷰']
+print(review)
+
+from sklearn.model_selection import train_test_split
